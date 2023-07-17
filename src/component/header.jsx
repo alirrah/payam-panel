d54da141154e0ff646520111db242ab5e54ce87e
alirrah <alir.rah81@gmail.com>
2023-07-17 15:13:12 +0330

    header

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,16 +1,64 @@
-import { Badge} from "antd";
-import { DashboardOutlined } from '@ant-design/icons';
+import { Badge, Dropdown, Divider, Avatar } from "antd";
+import { AimOutlined, ArrowDownOutlined, UserOutlined, DownOutlined, BellOutlined, InboxOutlined } from '@ant-design/icons';
 
+const items = [
+    {
+        label: <a href="https://www.antgroup.com">1st menu item</a>,
+        key: '0',
+    },
+    {
+        label: <a href="https://www.aliyun.com">2nd menu item</a>,
+        key: '1',
+    },
+    {
+        type: 'divider',
+    },
+    {
+        label: '3rd menu item',
+        key: '3',
+    },
+];
 
 function Header() {
     return (
         <header>
-        <img src={require('./../image/logo512.png')} />
-        <p>مرکز عملیات امنیت سایبری بانک ملت</p>
+            <img src={require('./../image/logo512.png')} />
+            <p>مرکز عملیات امنیت سایبری بانک ملت</p>
 
-        <Badge count={20} dot={false} offset={[20, -5]}>
-        <DashboardOutlined />
-    </Badge>
+            <Divider type="vertical" className="spacer" />
+
+            <div className="space row">
+            <p>5/h</p>
+            <AimOutlined className="redIcon"/>
+            </div>
+
+            <div className="space row">
+                <p>120/s</p>
+                <ArrowDownOutlined className="blueIcon"/>
+            </div>
+
+            <Divider type="vertical" />
+
+            <Badge count={20} className="notification space">
+                <BellOutlined />
+            </Badge>
+            <Badge count={20} className="notification space">
+                <InboxOutlined />
+            </Badge>
+
+            <Divider type="vertical" />
+
+            <Dropdown
+                menu={{
+                    items,
+                }}
+                className="space"
+            >
+                <a onClick={(e) => e.preventDefault()}>
+                    <Avatar size={30} icon={<UserOutlined />} />
+                    <DownOutlined className="arrowIcon" />
+                </a>
+            </Dropdown>
         </header>
     )
 }
--- src/component/menu.jsx
+++ src/component/menu.jsx
@@ -12,7 +12,7 @@ function getItem(label, key, icon, children, type) {
     };
 }
 const items = [
-    getItem('پیشخوان ها', 'sub1', <Badge count={20} dot={false} offset={[20, -5]}>
+    getItem('پیشخوان ها', 'sub1', <Badge count={20} offset={[20, -5]}>
         <DashboardOutlined />
     </Badge>, [
         getItem('Option 1', '1'),
@@ -20,14 +20,14 @@ const items = [
         getItem('Option 3', '3'),
         getItem('Option 4', '4'),
     ]),
-    getItem('گزارش ها', 'sub2', <Badge count={2} dot={false} offset={[20, -5]}>
+    getItem('گزارش ها', 'sub2', <Badge count={2} offset={[20, -5]}>
         <FundOutlined />
     </Badge>, [
         getItem('Option 5', '5'),
         getItem('Option 6', '6'),
         getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
     ]),
-    getItem('حوادث', 'sub4', <Badge count={0} dot={false} offset={[20, -5]}>
+    getItem('حوادث', 'sub4', <Badge count={0} offset={[20, -5]}>
         <AimOutlined />
     </Badge>, [
         getItem('Option 9', '9'),
@@ -35,7 +35,7 @@ const items = [
         getItem('Option 11', '11'),
         getItem('Option 12', '12'),
     ]),
-    getItem('رویدادها', 'sub5', <Badge count={9990} dot={false} offset={[20, -5]}>
+    getItem('رویدادها', 'sub5', <Badge count={9990} offset={[20, -5]}>
         <SecurityScanOutlined />
     </Badge>, [
         getItem('Option 9', '13'),
@@ -43,7 +43,7 @@ const items = [
         getItem('Option 11', '15'),
         getItem('Option 12', '16'),
     ]),
-    getItem('دانش', 'sub6', <Badge count={50} dot={false} offset={[20, -5]}>
+    getItem('دانش', 'sub6', <Badge count={50} offset={[20, -5]}>
         <BulbOutlined />
     </Badge>, [
         getItem('Option 9', '17'),
@@ -51,7 +51,7 @@ const items = [
         getItem('Option 11', '19'),
         getItem('Option 12', '20'),
     ]),
-    getItem('محیط', 'sub7', <Badge count={10} dot={false} offset={[20, -5]}>
+    getItem('محیط', 'sub7', <Badge count={10} offset={[20, -5]}>
         <DeploymentUnitOutlined />
     </Badge>, [
         getItem('Option 9', '21'),
@@ -59,7 +59,7 @@ const items = [
         getItem('Option 11', '23'),
         getItem('Option 12', '24'),
     ]),
-    getItem('تنظیمات', 'sub8', <Badge count={0} dot={false} offset={[20, -5]}>
+    getItem('تنظیمات', 'sub8', <Badge count={0} offset={[20, -5]}>
         <SettingOutlined />
     </Badge>, [
         getItem('Option 9', '25'),
