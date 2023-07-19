ce5bf1a7eccd0e2657ec52678b82760070a175df
alirrah <alir.rah81@gmail.com>
2023-07-19 13:53:40 +0330

    notification style

--- src/component/notification.jsx
+++ src/component/notification.jsx
@@ -4,27 +4,15 @@ import { LoginOutlined, AimOutlined, CloseCircleOutlined } from '@ant-design/ico
 
 const data = [
     {
-        icon: <AimOutlined
-            style={{
-                color: "#ff4d4f",
-                backgroundColor: "#ff4d4f22",
-                border: "1px solid",
-                borderRadius: "5px"
-            }}
-        />,
+        icon: <AimOutlined />,
+        danger: true,
         title: 'یک حادثه روی سرور ایمیل رخ داده است',
         time: '3 ثانیه پیش',
         description: 'حادثه از نوع باج افزار از مبدا 182.36.128.51 به سرور ایمیل شما می باشد.'
     },
     {
-        icon: <LoginOutlined
-            style={{
-                color: "#52c41a",
-                backgroundColor: "#52c41a22",
-                border: "1px solid",
-                borderRadius: "5px"
-            }}
-        />,
+        icon: <LoginOutlined />,
+        danger: false,
         title: 'ورود موفق به سامانه',
         danger: false,
         time: '3 ساعت پیش',
@@ -32,27 +20,15 @@ const data = [
 
     },
     {
-        icon: <LoginOutlined
-            style={{
-                color: "#ff4d4f",
-                backgroundColor: "#ff4d4f22",
-                border: "1px solid",
-                borderRadius: "5px"
-            }}
-        />,
+        icon: <LoginOutlined />,
+        danger: true,
         title: 'ورود ناموفق به سامانه',
         time: '5 ساعت پیش',
         description: 'ورود از آی پی 192.168.87.10 در تاریخ 12 مهر 1402 می باشد.'
     },
     {
-        icon: <LoginOutlined
-            style={{
-                color: "#52c41a",
-                backgroundColor: "#52c41a22",
-                border: "1px solid",
-                borderRadius: "5px"
-            }}
-        />,
+        icon: <LoginOutlined />,
+        danger: false,
         title: 'ورود موفق به سامانه',
         danger: false,
         time: '3 ساعت پیش',
@@ -83,12 +59,13 @@ function Notification() {
                             avatar={item.icon}
                             title={item.title}
                             description={item.time}
+                            className={(item.danger ? "danger" : "success")}
                         />
                         <div className='descriptionPart'>
-                        <p className='description'>{item.description}</p>
-                        <a href="#">بیشتر</a>
+                            <p className='description'>{item.description}</p>
+                            <a href="#">بیشتر</a>
                         </div>
-                        <CloseCircleOutlined className='closeBtn'/>
+                        <CloseCircleOutlined className='closeBtn' />
                     </List.Item>
                 )}
             />
