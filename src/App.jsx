dbd45c35fa7cc0f82e31636ed646c54ccb0381af
alirrah <alir.rah81@gmail.com>
2023-07-22 15:09:29 +0330

    get key of item in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import './App.css';
 import SidebarMenu from './component/menu';
 import Header from './component/header';
 import Content from './component/content'
+import { useState, createContext, useContext } from 'react';
+
 
 function App() {
+
+  const [number, setNumber] = useState(1);
+
   return (
     <>
-      <SidebarMenu />
+      <SidebarMenu context={(number) => {
+        setNumber(number)
+      }} />
       <section className='main'>
         <Header />
-        <Content />
+        <Content UserContext />
       </section>
     </>
   );
--- src/component/menu.jsx
+++ src/component/menu.jsx
@@ -3,68 +3,10 @@ import { DashboardOutlined, DeploymentUnitOutlined, AimOutlined, FundOutlined, B
 import { Menu } from 'antd';
 import DotedBadget from './doted-badge';
 
-function getItem(label, key, icon, children, type) {
-    return {
-        key,
-        icon,
-        children,
-        label,
-        type,
-    };
-}
-const items = [
-    getItem('پیشخوان ها', 'sub1', <DotedBadget count={45} icon={<DashboardOutlined />} />, [
-        getItem('مورد یک', '1'),
-        getItem('مورد دو', '2'),
-        getItem('مورد سه', '3'),
-        getItem('مورد چهار', '4'),
-    ]),
-    getItem('گزارش ها', 'sub2', <DotedBadget count={2} icon={<FundOutlined />} />, [
-        getItem('مورد پنج', '5'),
-        getItem('مورد شش', '6'),
-        getItem('زیر منو', 'sub3', null, [getItem('مورد هفت', '7'), getItem('مورد هشت', '8')]),
-    ]),
-    getItem('حوادث', 'sub4', <DotedBadget count={18} icon={<AimOutlined />} />, [
-        getItem('مورد نه', '9'),
-        getItem('مورد ده', '10'),
-        getItem('مورد یازده', '11'),
-        getItem('مورد دوازده', '12'),
-    ]),
-    getItem('رویدادها', 'sub5', <DotedBadget count={990} icon={<SecurityScanOutlined />} />, [
-        getItem('مورد سیزده', '13'),
-        getItem('مورد چهارده', '14'),
-        getItem('مورد پانزده', '15'),
-        getItem('مورد شانزده', '16'),
-    ]),
-    getItem('دانش', 'sub6', <DotedBadget count={0} icon={<BulbOutlined />} />, [
-        getItem('مورد هفده', '17'),
-        getItem('مورد هجده', '18'),
-        getItem('مورد نوزده', '19'),
-        getItem('مورد بیست', '20'),
-    ]),
-    getItem('محیط', 'sub7', <DotedBadget count={10} icon={<DeploymentUnitOutlined />} />, [
-        getItem('مورد بیست و یک', '21'),
-        getItem('مورد بیست و دو', '22'),
-        getItem('مورد بیست و سه', '23'),
-        getItem('مورد بیست و چهار', '24'),
-    ]),
-    getItem('تنظیمات', 'sub8', <DotedBadget count={0} icon={<SettingOutlined />} />, [
-        getItem('مورد بیست و پنج', '25'),
-        getItem('مورد بیست و شش', '26'),
-        getItem('مورد بیست و هفت', '27'),
-        getItem('مورد بیست وهشت', '28'),
-        getItem('مورد بیست و نه', '29'),
-        getItem('مورد سی', '30'),
-        getItem('مورد سی و یک', '31'),
-        getItem('مورد سی و دو', '32'),
-        getItem('مورد سی و سه', '33'),
-        getItem('مورد سی و چهار', '34'),
-    ]),
-];
-
 const rootSubmenuKeys = ['sub1', 'sub2', 'sub4', 'sub5', 'sub6', 'sub7', 'sub8'];
 
-function SidebarMenu() {
+function SidebarMenu(props) {
+
     const [openKeys, setOpenKeys] = useState(['sub1']);
     const onOpenChange = (keys) => {
         const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
@@ -77,6 +19,106 @@ function SidebarMenu() {
             setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
         }
     };
+
+    const getItem = (label, key, icon, children) => {
+        return {
+            key: key,
+            icon: icon,
+            children: children,
+            label: label,
+            onClick: () => (Number.isInteger(key) ? props.context(key) : null),
+        };
+    }
+
+    const items = [
+        getItem(
+            'پیشخوان ها',
+            'sub1',
+            <DotedBadget count={45} icon={<DashboardOutlined />} />,
+            [
+                getItem('مورد یک', 1, null, null),
+                getItem('مورد دو', 2, null, null),
+                getItem('مورد سه', 3, null, null),
+                getItem('مورد چهار', 4, null, null),
+            ],
+        ),
+        getItem(
+            'گزارش ها',
+            'sub2',
+            <DotedBadget count={2} icon={<FundOutlined />} />,
+            [
+                getItem('مورد پنج', 5, null, null),
+                getItem('مورد شش', 6, null, null),
+                getItem('زیر منو', 'sub3', null,
+                    [
+                        getItem('مورد هفت', 7, null, null),
+                        getItem('مورد هشت', 8, null, null),
+                    ],
+                ),
+            ],
+        ),
+        getItem(
+            'حوادث',
+            'sub4',
+            <DotedBadget count={18} icon={<AimOutlined />} />,
+            [
+                getItem('مورد نه', 9, null, null),
+                getItem('مورد ده', 10, null, null),
+                getItem('مورد یازده', 11, null, null),
+                getItem('مورد دوازده', 12, null, null),
+            ],
+        ),
+        getItem(
+            'رویدادها',
+            'sub5',
+            <DotedBadget count={990} icon={<SecurityScanOutlined />} />,
+            [
+                getItem('مورد سیزده', 13, null, null),
+                getItem('مورد چهارده', 14, null, null),
+                getItem('مورد پانزده', 15, null, null),
+                getItem('مورد شانزده', 16, null, null),
+            ]
+        ),
+        getItem(
+            'دانش',
+            'sub6',
+            <DotedBadget count={0} icon={<BulbOutlined />} />,
+            [
+                getItem('مورد هفده', 17, null, null),
+                getItem('مورد هجده', 18, null, null),
+                getItem('مورد نوزده', 19, null, null),
+                getItem('مورد بیست', 20, null, null),
+            ]
+        ),
+        getItem(
+            'محیط',
+            'sub7',
+            <DotedBadget count={10} icon={<DeploymentUnitOutlined />} />,
+            [
+                getItem('مورد بیست و یک', 21, null, null),
+                getItem('مورد بیست و دو', 22, null, null),
+                getItem('مورد بیست و سه', 23, null, null),
+                getItem('مورد بیست و چهار', 24, null, null),
+            ]
+        ),
+        getItem(
+            'تنظیمات',
+            'sub8',
+            <DotedBadget count={0} icon={<SettingOutlined />} />,
+            [
+                getItem('مورد بیست و پنج', 25, null, null),
+                getItem('مورد بیست و شش', 26, null, null),
+                getItem('مورد بیست و هفت', 27, null, null),
+                getItem('مورد بیست وهشت', 28, null, null),
+                getItem('مورد بیست و نه', 29, null, null),
+                getItem('مورد سی', 30, null, null),
+                getItem('مورد سی و یک', 31, null, null),
+                getItem('مورد سی و دو', 32, null, null),
+                getItem('مورد سی و سه', 33, null, null),
+                getItem('مورد سی و چهار', 34, null, null),
+            ],
+        ),
+    ];
 
     return (
         <section className='sidebar'>
