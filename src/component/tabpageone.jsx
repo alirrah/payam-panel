3f68adfc3a67cd049db5846b2e70e68aefa51cf5
alirrah <alir.rah81@gmail.com>
2023-07-24 09:56:36 +0330

    title page initial value

--- src/component/tabpageone.jsx
+++ src/component/tabpageone.jsx
@@ -4,55 +4,54 @@ import { useState } from 'react';
 
 function TabPage(props) {
 
-    const [title, setTitle] = useState('تجهیزات')
+    const [title, setTitle] = useState(props.items[0].label)
 
     const onChange = (key) => {
         setTitle(props.items[key - 1].label)
     };
 
     return (
-        <>
-            <section className="content">
-                <Tabs defaultActiveKey="1" items={props.items} onChange={onChange}/>
+        <section className="content">
+            <Tabs defaultActiveKey="1" items={props.items} onChange={onChange} />
 
-                <section className="tabPage" >
-                    <h1>{title}</h1>
-                    <Breadcrumb
-                        separator=">"
-                        items={[
-                            {
-                                title: 'پایگاه دانش',
-                            },
-                            {
-                                title: 'قوانین تحلیل',
-                                href: '',
-                            },
-                            {
-                                title: 'قوانین وارسی',
-                                href: '',
-                            },
-                        ]}
-                    />
-                    <Button className='btn spacer square'>
-                        <SaveOutlined />
-                    </Button>
-                    <Button className='btn square'>
-                        <FilterOutlined />
-                    </Button>
-                    <Button className='btn square'>
-                        <DownloadOutlined />
-                    </Button>
-                    <Button className='btn square'>
-                        <ReloadOutlined />
-                    </Button>
-                    <Button type="primary" className='btn primaryBtn'>
-                        <PlusOutlined />
-                        افزودن آیتم
-                    </Button>
-                </section>
+            <section className="tabPage" >
+                <h1>{title}</h1>
+                <Breadcrumb
+                    separator=">"
+                    items={[
+                        {
+                            title: 'پایگاه دانش',
+                        },
+                        {
+                            title: 'قوانین تحلیل',
+                            href: '',
+                        },
+                        {
+                            title: 'قوانین وارسی',
+                            href: '',
+                        },
+                    ]}
+                />
 
+                <Button className='btn spacer square'>
+                    <SaveOutlined />
+                </Button>
+                <Button className='btn square'>
+                    <FilterOutlined />
+                </Button>
+                <Button className='btn square'>
+                    <DownloadOutlined />
+                </Button>
+                <Button className='btn square'>
+                    <ReloadOutlined />
+                </Button>
+                <Button type="primary" className='btn primaryBtn'>
+                    <PlusOutlined />
+                    افزودن آیتم
+                </Button>
             </section>
-        </>
+
+        </section>
     )
 }
 
